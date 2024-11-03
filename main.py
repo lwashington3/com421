b86e5012236ebe0efff912a843cfaffeaf694f83
Len Washington III <77632128+lwashington3@users.noreply.github.com>
2024-11-03 16:52:49 -0600

    Website should be ready.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-import ssl
-import socketserver
+from ssl import SSLContext, PROTOCOL_TLS_SERVER
+
+
+class Handler(SimpleHTTPRequestHandler):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, directory="public_html", **kwargs)
 
 
 def http():
-	with HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler) as httpd:
+	with HTTPServer(('', 80), Handler) as httpd:
 		httpd.serve_forever()
 
 
 def https():
-	with HTTPServer(('0.0.0.0', 443), SimpleHTTPRequestHandler) as httpd:
-		context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+	with HTTPServer(('', 443), Handler) as httpd:
+		context = SSLContext(PROTOCOL_TLS_SERVER)
 		context.load_cert_chain('certs/cert.pem', 'certs/key.pem', "genericpassword!")
 
 		httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
