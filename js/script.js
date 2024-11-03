574c1da8986efd4b4640505dbb70a53f24dc7878
Len Washington III <77632128+lwashington3@users.noreply.github.com>
2024-11-03 13:06:49 -0600

    Updated the formatting for the CSS file. Allowed for invalid pages to still have icons. The figures will now have a figure number added through JS.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
+function onLoad(published_only){
+	addNav(published_only);
+	console.log("Should be added");
+	addFigureNumbers();
+}
+
+
 function addNav(published_only){
 	const menu = document.querySelector("nav.top-level");
 	const pages = {
 		"Home": ["./index.html", "fa-house"],
 		"Topic": null,
-		"Research": ["./realistic_research.html", "fa-book"],
+		"Research": ["./realistic_research.html", "fa-microscope"],
 		"Data": ["./formatting_research_data.html", "fa-calculator"],
-		"References": null,
-		"Style": null,
+		"References": [null, "fa-book"],
+		"Style": [null, "fa-paintbrush"],
 		"Layout": null,
-		"Modality": null,
+		"Modality": [null, "fa-boxes-stacked"],
 		"Professionalization": null,
 	};
 	// const list = document.createElement("ul");
@@ -17,12 +24,14 @@ function addNav(published_only){
 	bar.setAttribute("class", "navbar");
 	for (const page in pages) {
 		const info = pages[page];
-		if(published_only && info == null){ continue; }
+		let valid = !(info == null || info[0] == null);
+		if(published_only && valid){ continue; }
 		let url = "./placeholder.html";
 		let iconValue = "fa-x";
-		let valid = info != null;
 		if(valid){
 			url = info[0];
+		}
+		if(info != null){
 			iconValue = `fa-solid ${info[1]}`;
 		}
 		const link = document.createElement("a");
@@ -60,4 +69,11 @@ function addNav(published_only){
 		bar.append(link);
 	}
 	menu.appendChild(bar);
+}
+
+function addFigureNumbers(){
+	const captions = document.querySelectorAll("figcaption");
+	for(let i = 0; i < captions.length; i++){
+		captions[i].innerText = `Figure ${i+1}: ${captions[i].innerText}`;
+	}
 }
